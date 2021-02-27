88fb1e60d0d0d48f5fcba02ffe15bc8ca6d114a7
SanPedro-Sebastian-SD2A <66133774+SanPedro-Sebastian-SD2A@users.noreply.github.com>
2021-02-27 21:40:36 +0800

    Add files via upload
    Activities 1-3

---
+++ Sample2_MyProfile/SanPedroSebastian.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample2_MyProfile
+{
+    class SanPedroSebastian
+    {
+        /* Name: Sebastian Miguel S. San Pedro
+         * Date: of Birth: August 6
+         * Year: II
+         * Section: SD2A
+         */
+        static void Main(string[] args)
+        {
+            string Name = "Sebastian Miguel S. San Pedro";
+            Console.WriteLine("Name: " + Name);
+
+            string DateOfBirth = "August 6";
+            Console.WriteLine("Date of Birth: " + DateOfBirth);
+
+            string Year = "II";
+            Console.WriteLine("Year: " + Year);
+
+            string Section = "SD2A";
+            Console.WriteLine("Section: " + Section);
+        }
+    }
+}
---
+++ Sample3_InputMyName/SanPedroSebastian.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample3_InputMyName
+{
+    class SanPedroSebastian
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name (firstname lastname): ");
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("\n Hello " + Name + "!!!");
+            Console.WriteLine(" Welcome to OOP environment.");
+        }
+    }
+}
