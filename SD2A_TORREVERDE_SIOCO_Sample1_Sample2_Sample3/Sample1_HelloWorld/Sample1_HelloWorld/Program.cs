bc645d79453840356b1b9afa05ae6efc5670ef2e
lbctorreverde <79499382+lbctorreverde@users.noreply.github.com>
2021-03-01 14:53:31 +0800

    Add files via upload

---
+++ SD2A_TORREVERDE_SIOCO_Sample1_Sample2_Sample3/Sample1_HelloWorld/Sample1_HelloWorld/Program.cs
@@ -0,0 +1,30 @@
+﻿/* 19-01073 
+ * Torreverde, Leonard Bryan C.
+ * 
+ * 19-00735
+ * Sioco, John Kyle D.
+ * 
+ * SD2A
+ * March 2, 2021
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample1_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+
+        }
+
+    }
+}
