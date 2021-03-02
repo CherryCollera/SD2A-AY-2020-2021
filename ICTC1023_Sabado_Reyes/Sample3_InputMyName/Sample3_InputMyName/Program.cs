bb8681f248f9d6a70f8d696e1a80d9468e8a5e49
SabadoJaydeeM <79853632+SabadoJaydeeM@users.noreply.github.com>
2021-03-02 09:46:49 +0800

    Add files via upload

---
+++ ICTC1023_Sabado_Reyes/Sample3_InputMyName/Sample3_InputMyName/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+namespace Sample3_InputMyName
+{
+    class Person
+    {
+        private string Fullname;
+
+        //Method for getter and setter
+        public string FullName
+        {
+            get { return Fullname; }
+            set { Fullname = value; }
+        }
+
+        //Welcome message
+        public void Welcome()
+        {
+            Console.WriteLine("\n Hello " + FullName + "!!!\n Welcome to OOP environment");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //object creation for the Person class
+            Person obj = new Person();
+
+            Console.Write("Enter your name (firstname lastname)");
+
+            //Reading user input as value for the setter method
+            obj.FullName = Console.ReadLine();
+
+            obj.Welcome();
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
